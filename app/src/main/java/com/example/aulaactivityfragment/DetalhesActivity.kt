9b70d4d889d7b0d402168e4251102b50f64628a2
Math012 <math111.0811@gmail.com>
2025-04-10 15:25:22 -0300

    Enviando dados entre activities

---
+++ app/src/main/java/com/example/aulaactivityfragment/DetalhesActivity.kt
@@ -0,0 +1,47 @@
+package com.example.aulaactivityfragment
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var buttonFechar: Button
+    lateinit var textFilme: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalhes)
+
+        buttonFechar = findViewById(R.id.button_fechar)
+        textFilme = findViewById(R.id.textFilme)
+
+        // Recuperando dados de outra activity
+        val bundle = intent.extras
+        if ( bundle != null){
+            val filme = bundle.getString("filme")
+            val classificação = bundle.getInt("classificação")
+            val avaliações = bundle.getDouble("avaliações")
+
+            val resultado = "filme: $filme , classificação: $classificação , avaliação: $avaliações"
+
+            textFilme.text = resultado
+        }
+
+        buttonFechar.setOnClickListener {
+            finish()
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aulaactivityfragment/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.aulaactivityfragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonAbrir: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        buttonAbrir = findViewById(R.id.button_abrir)
+        buttonAbrir.setOnClickListener {
+
+            val intent = Intent(this, DetalhesActivity::class.java)
+
+            // Passando parametros para uma nova activity
+            intent.putExtra("filme","The Witcher")
+            intent.putExtra("classificação",5)
+            intent.putExtra("avaliações",9.2)
+
+
+            intent.putExtra("filme", filme)
+
+            // Iniciando uma nova activity
+            startActivity( intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
